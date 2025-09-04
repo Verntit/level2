78a97205b8a1d20733bd83775ae8f638e1bfa544
Verntit <ht32@mahurangi.school.nz>
2025-09-05 03:28:36 +1200

    Finished the vanishing platform

--- SCRIPT/Vanishing platform.gd
+++ SCRIPT/Vanishing platform.gd
@@ -2,14 +2,25 @@ extends StaticBody2D
 
 @onready var timer = $VanishingTimer
 @onready var collision_shape_2d = $CollisionShape2D
+@onready var reset_timer = $Reset_Timer
 
+var original_modulate_a: float
+
+func _ready():
+	original_modulate_a = modulate.a
 
 func _on_trigger_area_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and timer.is_stopped(): 
 		timer.start()
-	
-
+		reset_timer.start()
+		
 func _on_vanishing_timer_timeout():
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 0, 0.4) 
-	collision_shape_2d.position = Vector2(-1000000, 200000)
+	collision_shape_2d.set_deferred("disabled", true) 
+
+func _on_reset_timer_timeout():
+		# Reset the platform after 5 seconds
+	collision_shape_2d.set_deferred("disabled", false) 
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", original_modulate_a, 0.4)
